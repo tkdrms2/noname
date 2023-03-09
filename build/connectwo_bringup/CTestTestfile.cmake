8e091ecbb7c8b9729e2296c81c942a7e632ae040
tkdrms2 <trcsam09@gmail.com>
2023-03-09 15:13:08 +0900

    commit message

---
+++ build/connectwo_bringup/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/shin/catkin_ws/src/connectwo_bringup
+# Build directory: /home/shin/catkin_ws/build/connectwo_bringup
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
---
+++ devel/share/turtlebot3_msgs/cmake/turtlebot3_msgs-msg-extras.cmake
@@ -0,0 +1,2 @@
+set(turtlebot3_msgs_MESSAGE_FILES "/home/shin/catkin_ws/src/turtlebot3_msgs/msg/SensorState.msg;/home/shin/catkin_ws/src/turtlebot3_msgs/msg/VersionInfo.msg;/home/shin/catkin_ws/src/turtlebot3_msgs/msg/Sound.msg")
+set(turtlebot3_msgs_SERVICE_FILES "")
